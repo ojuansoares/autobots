1d89075c362a31d515ae0d74c6fb1363a467579f
Juan Soares <juan.garcia.soares@gmail.com>
2024-11-06 14:00:00 -0300

    chore: Remove atvi and atvii from main branch

--- atvi/src/main/java/com/autobots/atvi/repositorios/RepositorioEndereco.java
+++
@@ -1,9 +0,0 @@
-package com.autobots.atvi.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.autobots.atvi.entidades.Endereco;
-
-public interface RepositorioEndereco extends JpaRepository<Endereco, Long>{
-    
-}
