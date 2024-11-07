5288ce73efc50506395f6649cf8036fbb7c3a7fe
Juan Soares <juan.garcia.soares@gmail.com>
2024-11-07 14:00:00 -0300

    feat: Add endpoints for Documento, Endereco and Telefone

--- atvi/src/main/java/com/autobots/atvi/entidades/Documento.java
+++ atvi/src/main/java/com/autobots/atvi/entidades/Documento.java
@@ -1,10 +1,14 @@
 package com.autobots.atvi.entidades;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Documento {
@@ -12,11 +16,16 @@ public class Documento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(nullable = false)
     private String tipo;
 
-    @Column
+    @Column(nullable = false, unique = true)
     private String numero;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id", nullable = false)
+    @JsonBackReference
+    private Cliente cliente;
 
     public Long getId() {
         return id;
@@ -41,4 +50,14 @@ public class Documento {
     public void setNumero(String numero) {
         this.numero = numero;
     }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    
 }
