dde7a4ec830bf94cfc0f0f7deaeacbc9cedff2cf
Juan Soares <juan.garcia.soares@gmail.com>
2024-11-08 14:00:00 -0300

    chore[atvii]: Initial setup

---
+++ atvii/src/test/java/com/autobots/atvi/AtviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autobots.atvi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
