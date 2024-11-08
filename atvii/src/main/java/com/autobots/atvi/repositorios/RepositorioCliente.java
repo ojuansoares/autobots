d3d47fd3e4ad125eb23c341a3862612cd902fcf2
Juan Soares <juan.garcia.soares@gmail.com>
2024-11-08 14:00:00 -0300

    chore: Remove atvi and atvii from main branch

--- atvii/src/main/java/com/autobots/atvi/repositorios/RepositorioCliente.java
+++
@@ -1,9 +0,0 @@
-package com.autobots.atvi.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.autobots.atvi.entidades.Cliente;
-
-public interface RepositorioCliente extends JpaRepository<Cliente, Long> {
-    
-}
