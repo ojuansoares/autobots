d205de77326cc35bb4965b2ed5a80353b0c6c0aa
Juan Soares <juan.garcia.soares@gmail.com>
2024-11-16 14:00:00 -0300

    feat: ATVII Complete

---
+++ automanager/src/main/java/com/autobots/automanager/controles/DocumentoControle.java
@@ -0,0 +1,103 @@
+package com.autobots.automanager.controles;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.autobots.automanager.entidades.Documento;
+import com.autobots.automanager.modelos.AdicionadorLinkDocumento;
+import com.autobots.automanager.modelos.DocumentoAtualizador;
+import com.autobots.automanager.repositorios.DocumentoRepositorio;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class DocumentoControle {
+    @Autowired
+    private DocumentoRepositorio repositorio;
+
+    @Autowired
+    private AdicionadorLinkDocumento adicionadorLink;
+
+    @Autowired
+    private DocumentoAtualizador atualizador;
+
+    @GetMapping("/documento/{id}")
+    public ResponseEntity<?> obterDocumento(@PathVariable Long id) {
+        try {
+            if (!repositorio.findById(id).isPresent()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+            Documento documento = repositorio.findById(id).get();
+            adicionadorLink.adicionarLink(documento);
+
+            return new ResponseEntity<>(documento, HttpStatus.FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/documentos")
+    public ResponseEntity<?> obterDocumentos() {
+        try {
+            if (repositorio.findAll().isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            
+            List<Documento> documentos = repositorio.findAll();
+            adicionadorLink.adicionarLink(documentos);
+
+            return new ResponseEntity<>(documentos, HttpStatus.FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    @PostMapping("/documento/cadastro")
+    public ResponseEntity<?> cadastrarDocumento(@RequestBody Documento documento) {
+        try {
+            repositorio.save(documento);
+
+            adicionadorLink.adicionarLink(documento);
+
+            return new ResponseEntity<>(documento, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/documento/atualizar")
+    public ResponseEntity<?> atualizarDocumento(@RequestBody Documento atualizacao) {
+        try {
+            if (!repositorio.findById(atualizacao.getId()).isPresent()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            
+            Documento documento = repositorio.findById(atualizacao.getId()).get();
+            atualizador.atualizar(documento, atualizacao);
+            repositorio.save(documento);
+
+            adicionadorLink.adicionarLink(documento);
+
+            return new ResponseEntity<>(documento, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/documento/excluir")
+    public ResponseEntity<?> deletarDocumento(@RequestBody Documento exclusao) {
+        try {
+            if (!repositorio.findById(exclusao.getId()).isPresent()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            Documento documento = repositorio.findById(exclusao.getId()).get();
+            repositorio.delete(documento);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ automanager/src/main/java/com/autobots/automanager/controles/TelefoneControle.java
@@ -0,0 +1,101 @@
+package com.autobots.automanager.controles;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.autobots.automanager.entidades.Telefone;
+import com.autobots.automanager.modelos.AdicionadorLinkTelefone;
+import com.autobots.automanager.modelos.TelefoneAtualizador;
+import com.autobots.automanager.repositorios.TelefoneRepositorio;
+
+@RestController
+public class TelefoneControle {
+    @Autowired
+    private TelefoneRepositorio repositorio;
+
+    @Autowired
+    private TelefoneAtualizador atualizador;
+
+    @Autowired
+    private AdicionadorLinkTelefone adicionadorLink;
+
+    @GetMapping("/telefone/{id}")
+    public ResponseEntity<?> obterTelefone(@PathVariable Long id) {
+        try {
+            if (!repositorio.findById(id).isPresent()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+            Telefone telefone = repositorio.findById(id).get();
+            adicionadorLink.adicionarLink(telefone);
+
+            return new ResponseEntity<>(telefone, HttpStatus.FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/telefones")
+    public ResponseEntity<?> obterTelefones() {
+        try {
+            if (repositorio.findAll().isEmpty()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            
+            List<Telefone> telefones = repositorio.findAll();
+            adicionadorLink.adicionarLink(telefones);
+
+            return new ResponseEntity<>(telefones, HttpStatus.FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/telefone/cadastro")
+    public ResponseEntity<?> cadastrarTelefone(@RequestBody Telefone telefone) {
+        try {
+            repositorio.save(telefone);
+
+            adicionadorLink.adicionarLink(telefone);
+
+            return new ResponseEntity<>(telefone, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/telefone/atualizar")
+    public ResponseEntity<?> atualizarTelefone(@RequestBody Telefone atualizacao) {
+        try {
+            if (!repositorio.findById(atualizacao.getId()).isPresent()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            
+            Telefone telefone = repositorio.findById(atualizacao.getId()).get();
+            atualizador.atualizar(telefone, atualizacao);
+            repositorio.save(telefone);
+
+            adicionadorLink.adicionarLink(telefone);
+
+            return new ResponseEntity<>(telefone, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/telefone/excluir")
+    public ResponseEntity<?> deletarTelefone(@PathVariable Telefone exclusao) {
+        try {
+            if (!repositorio.findById(exclusao.getId()).isPresent()) return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            Telefone telefone = repositorio.findById(exclusao.getId()).get();
+            repositorio.delete(telefone);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
